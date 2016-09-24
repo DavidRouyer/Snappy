c96d3bf9e1497252c4df01ca911867f9927196cb
David Rouyer <rouyer.david@gmail.com>
2016-09-24 18:47:33 +0200

    Change library ID from Snappy to Snappy.Standard

--- src/Snappy/Properties/AssemblyInfo.cs
+++ src/Snappy/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Snappy for .NET Standard")]
 [assembly: AssemblyDescription(".NET Standard client for Google's Snappy compression library, inspired by Snappy.NET. Snappy is a very fast compression(250 MB/sec)/decompression(500 MB/sec) library written in C++.")]
 [assembly: AssemblyCompany("David Rouyer")]
-[assembly: AssemblyProduct("Snappy")]
+[assembly: AssemblyProduct("Snappy.Standard")]
 
 [assembly: InternalsVisibleTo("Snappy.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
